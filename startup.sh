0ca83e2668b69bc62a1cb48e15a4ef3f91965fb9
vvka <vvka@users.noreply.github.com>
2017-01-23 13:17:11 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gem install sinatra
+if [ "$RACK_ENV" == "production" ]; 
+then 
+  bundle install --without development test
+  ruby $MAIN_APP_FILE -p 80
+else
+  bundle install
+  if [ "$RACK_ENV" == "test" ]; 
+  then
+    rspec
+  else
+    gem install shotgun
+    shotgun -I /usr/src/app $MAIN_APP_FILE -p 80 -o '0.0.0.0'
+  fi
+fi
